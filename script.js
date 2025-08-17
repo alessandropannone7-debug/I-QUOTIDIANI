02cff3705f706398008bad706c280ac7d54c472d
alessandropannone7-debug <alessandropannone7@gmail.com>
2025-08-17 18:41:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,17 @@ function startOnce() {
   if (!started) {
     audio.play().then(() => updateButton()).catch(()=>{});
     started = true;
+    const rain = document.getElementById('rain');
+const numberOfDrops = 100;
+
+for(let i=0; i<numberOfDrops; i++){
+  const drop = document.createElement('div');
+  drop.className = 'drop';
+  drop.style.left = Math.random() * 100 + 'vw';
+  drop.style.animationDuration = 0.5 + Math.random() * 0.5 + 's';
+  drop.style.animationDelay = Math.random() * 2 + 's';
+  rain.appendChild(drop);
+}
   }
 }
 window.addEventListener('pointerdown', startOnce, { once: true });
